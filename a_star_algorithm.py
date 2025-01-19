289015f1415ca5a4a4ac6ca0cb7ec5f6a565085d
SehanAF <sehanaf4@gmail.com>
2025-01-19 11:20:04 +0700

    Update: fix logic heuristic

--- a_star_algorithm.py
+++ a_star_algorithm.py
@@ -1,39 +1,39 @@
 import heapq
 
 graph = {
-    'RS': {'E': 3},
-    'E': {'RS': 3, 'I': 2},
-    'I': {'E': 2, 'J': 4, 'L': 6},
-    'L': {'I': 6, 'K': 4},
-    'K': {'L': 4},
-    'J': {'I': 4},
+    'RS': {'E': 3, 'A': 7, 'B': 2},
     'A': {'RS': 7, 'B': 3, 'D': 4},
-    'B': {'A': 3, 'C': 1},
-    'C': {'B': 1, 'D': 6, 'F': 3},
-    'D': {'A': 4, 'C': 6},
+    'B': {'RS': 2, 'A': 3, 'C': 1},
+    'C': {'B': 1, 'G': 2, 'F': 3},
+    'D': {'A': 4, 'B': 4, 'F': 5},
+    'E': {'RS': 3, 'I': 2},
+    'I': {'E': 2, 'J': 4, 'L': 4},
+    'L': {'I': 4, 'J': 6, 'K': 4},
+    'K': {'L': 4, 'G': 3},
+    'J': {'I': 4, 'L': 6},
     'F': {'C': 3, 'G': 2},
     'G': {'F': 2},
 }
 
 heuristics = {
-    'RS': 0, 'E': 3, 'I': 6, 'L': 10, 'K': 13,
-    'J': 7, 'A': 22, 'B': 18, 'C': 13, 'D': 20,
-    'F': 11, 'G': 13,
+    'RS': 0, 'E': 8, 'I': 6, 'L': 4, 'K': 5,
+    'J': 4, 'A': 9, 'B': 7, 'C': 6, 'D': 8,
+    'F': 6, 'G': 13,
 }
 
 def a_star_algorithm(graph, heuristics, start, goal):
     open_list = []
     closed_list = set()
-    
-    heapq.heappush(open_list, (0 + heuristics[start], 0, start, []))  # (f, g, current, path)
+    heapq.heappush(open_list, (heuristics[start], 0, start, []))  # (f, g, current, path)
 
     while open_list:
         f, g, current, path = heapq.heappop(open_list)
-
         if current == goal:
-            return path + [current], g
+            total_cost = g + heuristics[start]  
+            return path + [current], total_cost
+       
         closed_list.add(current)
-        
+
         for neighbor, cost in graph[current].items():
             if neighbor in closed_list:
                 continue
@@ -45,7 +45,10 @@ def a_star_algorithm(graph, heuristics, start, goal):
 
     return None, float('inf')
 
-shortest_path, total_cost = a_star_algorithm(graph, heuristics, 'RS', 'I')
+start_node = 'I'  
+goal_node = 'RS'  
+
+shortest_path, total_cost = a_star_algorithm(graph, heuristics, start_node, goal_node)
 
 print("Jalur terpendek:", " -> ".join(shortest_path))
 print("Biaya total:", total_cost)
